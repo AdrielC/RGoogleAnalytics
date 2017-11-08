db31c485e940d52d6bf454538b4fe2c41c47fd8a
Adriel Casellas <adrielcasellas@gmail.com>
2017-11-08 11:33:55 -0700

    update 2

--- setup.R
+++ setup.R
@@ -1,5 +1,6 @@
-token <- Auth("Your_ClientID", "ClientSecret") # first Client ID, then Client Secret
+token <- Auth("294470013201-qrdobt7mfpffbvpcfsfsap74grr94566.apps.googleusercontent.com", "osOqE2SI8bj8obDz8GkP_B_9") # first Client ID, then Client Secret
 
 save(token,file="./token_file") # This will save the token on your computer so you do not need to authenticate everytime
 
-ValidateToken(token) # Checks if the token is valid
+ValidateToken(token)
+
